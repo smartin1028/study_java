cff04863c4ae74cdc38c8ffd830afc6e954c3f2e
daewon <leedw3232@gmail.com>
2024-12-06 07:05:30 +0900

    로그 테스트 정보

---
+++ src/test/java/org/study/green/LoggerTest.java
@@ -0,0 +1,35 @@
+package org.study.green;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerTest {
+
+
+    // SLF4J 로거 인스턴스 생성
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Test
+    public void t_LoggerTest_true_00() {
+
+        // 다양한 로그 레벨 사용
+        logger.trace("TRACE 레벨 메시지");
+        logger.debug("DEBUG 레벨 메시지");
+        logger.info("INFO 레벨 메시지");
+        logger.warn("WARN 레벨 메시지");
+        logger.error("ERROR 레벨 메시지");
+
+        // 파라미터화된 로깅
+        String name = "SLF4J";
+        int value = 42;
+        logger.info("파라미터 예제: name={}, value={}", name, value);
+
+        // 예외 로깅
+        try {
+            throw new RuntimeException("테스트 예외");
+        } catch (Exception e) {
+            logger.error("에러 발생", e);
+        }
+    }
+}
--- src/test/java/org/study/green/stream/AggregateFunctionsTestTest.java
+++
@@ -1,7 +0,0 @@
-package org.study.green.stream;
-
-import junit.framework.TestCase;
-
-public class AggregateFunctionsTestTest extends TestCase {
-
-}
