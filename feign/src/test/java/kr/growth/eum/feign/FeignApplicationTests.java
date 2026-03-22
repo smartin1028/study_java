83e148c0250b5ece4e1560a19e4eba88b6e70cca
dwsamba <leedw3232@gmail.com>
2026-03-22 12:18:05 +0900

    feign test init

---
+++ feign/src/test/java/kr/growth/eum/feign/FeignApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.growth.eum.feign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
